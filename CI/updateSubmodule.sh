6936cd5935ab0e71a1ecbbb0a4ac757ccd5d794d
bhamacher <b.hamacher@zera.de>
2021-10-12 06:36:01 +0200

    Update all submodules with changes instead of only one.
    second race condition fix.
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- CI/updateSubmodule.sh
+++ CI/updateSubmodule.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-submodule=${1}
+
+newCommits="$(git status | grep 'new commits')"
 workroot=$(pwd)
-submodulepath=$(grep path .gitmodules | sed 's/.* //' | grep $submodule)
-branch=$(grep branch .gitmodules | sed 's/.* //' | grep $submodule)
-if [ -z "$branch" ]
-then
+while read -r path
+do
+    submodulepath=$(echo $path | sed -re 's/^\s*(\S*)\s*(\S*).*/\2/g')
+    submodule=${submodulepath##*/} 
+    branch=$(grep branch .gitmodules | sed 's/.* //' | grep $submodule)
+    if [ -z "$branch" ]
+    then
       branch="master"
-fi
-if [ -z "$submodulepath" ]
-then
-      exit 1
-fi
-cd $submodulepath
-git checkout $branch
-cd $workroot
-currentid=$(git rev-parse HEAD:$submodulepath)
-git -c "user.name=gitbot" -c "user.email=gitbot" submodule update --init --remote --rebase -- $submodulepath
-git add $submodulepath
-cd $submodulepath
-reflogmsg=$(git log --pretty=format:"%h %s" $currentid..)
-cd $workroot
-git -c "user.name=gitbot" -c "user.email=gitbot@noreply.de"  commit -s -m "update $submodule" -m "$reflogmsg"
+    fi
+    if [ -z "$submodulepath" ]
+    then
+        exit 1
+    fi
+    cd $submodulepath
+    git checkout $branch
+    cd $workroot
+    currentid=$(git rev-parse HEAD:$submodulepath)
+    git -c "user.name=gitbot" -c "user.email=gitbot" submodule update --init --remote --rebase -- $submodulepath
+    git add $submodulepath
+    cd $submodulepath
+    reflogmsg=$(git log --pretty=format:"%h %s" $currentid..)
+    cd $workroot
+    git -c "user.name=gitbot" -c "user.email=gitbot@noreply.de"  commit -s -m "update $submodule" -m "$reflogmsg"
+done < <(echo "$newCommits")
